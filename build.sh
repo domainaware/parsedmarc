02944a8f70bd65e169a7e168a6771c9e2dee3cb7
Sean Whalen <whalenster@gmail.com>
2018-03-27 10:43:26 -0400

    Add pip to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 . ~/venv/domainaware/bin/activate
+pip install -U -r requirements.txt
 cd docs && make html && cp -r _build/html/* ../../parsedmarc-docs/
 cd ..
 rm -rf dist/ build/
