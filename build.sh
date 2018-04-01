a2bbc0effceb60096cb54b0f6b3619a46326d67b
Sean Whalen <whalenster@gmail.com>
2018-03-31 23:05:14 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 . ~/venv/domainaware/bin/activate
 pip install -U -r requirements.txt
+rstcheck README.rst docs/index.rst
 cd docs && make html && cp -r _build/html/* ../../parsedmarc-docs/
 cd ..
 rm -rf dist/ build/
