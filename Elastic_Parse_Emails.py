bb3025c1d24ca531d8477f9351a7dde287295c65
JayBuckley7 <ghfghf77@gmail.com>
2018-09-05 09:49:14 -0400

    Update Elastic_Parse_Emails.py
    no more pesky publicsuffixdownload

--- Elastic_Parse_Emails.py
+++ Elastic_Parse_Emails.py
@@ -2,10 +2,16 @@ import unittest
 import parsedmarc.elastic
 from glob import glob
 import json
-
 import parsedmarc
+import urllib2
 
 
+datFile = urllib2.urlopen("https://publicsuffix.org/list/public_suffix_list.dat")
+with open('.public_suffix_list.dat','wb') as output:
+  output.write(datfile.read())
+
+
+  
 class Test(unittest.TestCase):
     def testSamples(self):
         parsedmarc.elastic.set_hosts("localhost:9200")##set host
