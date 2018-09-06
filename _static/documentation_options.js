74a8a2aec2ded3cf7058263c6aa4b38851ce7a15
Sean Whalen <whalenster@gmail.com>
2018-09-06 11:18:02 -0400

    3.9.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.8.2',
+    VERSION: '3.9.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
