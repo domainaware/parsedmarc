ee15d213209662a118bccb1d500eab7ae2977384
Jacob A Buckley <s292155@corp.aepsc.com>
2018-09-06 11:33:15 -0400

    args added

--- Elastic_Parse_Emails.py
+++ Elastic_Parse_Emails.py
@@ -1,23 +1,40 @@
-import unittest
 import parsedmarc.elastic
 from glob import glob
 import json
 import parsedmarc
 import urllib
+from argparse import ArgumentParser
 
+
+
+arg_parser = ArgumentParser(description="Parses DMARC reports into elastic search")
+arg_parser.add_argument("-E", "--elasticsearch-host", nargs="*",
+                            help="A list of one or more Elasticsearch "
+                                 "hostnames or URLs to use (Default "
+                                 "localhost:9200)",
+                            default=["localhost:9200"])                            
+arg_parser.add_argument("-H", "--host", help="IMAP hostname or IP address")
+arg_parser.add_argument("-U", "--user", help="IMAP user")
+arg_parser.add_argument("-P", "--password", help="IMAP password")
+
+args = arg_parser.parse_args()
+
+try:
+    if args.host is None or args.user is None or args.password is None:
+        print("user and password must be specified if host is specified")
+        print("Ex: -E localhost:9200 -H 'email.com' -U 'myemail' -P 'mypassword'  " )
+except:
+    print("")
 
 datFile = urllib.request.urlopen("https://publicsuffix.org/list/public_suffix_list.dat")
 with open('.public_suffix_list.dat','wb') as output:
   output.write(datFile.read())
 
 
-
-  
-class Test(unittest.TestCase):
-    def testSamples(self):
-        parsedmarc.elastic.set_hosts("localhost:9200")##set host
-        parsedmarc.elastic.create_indexes()      
-        temp = parsedmarc.watch_inbox("host.email.com","username","password")##set server,user,pass
-        
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+parsedmarc.elastic.set_hosts(args.elasticsearch_host)
+parsedmarc.elastic.create_indexes() 
+host = args.host[0]
+user = args.user[0]
+password = args.password[0]     
+temp = parsedmarc.parse_watched_inbox(host,user,password)
+          
