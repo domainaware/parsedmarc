8ea0e62bdd0f1b95e6ac0a166da884f8a500d1de
Sean Whalen <whalenster@gmail.com>
2018-10-09 20:21:17 -0400

    Add more debug logging details

--- parsedmarc/splunk.py
+++ parsedmarc/splunk.py
@@ -61,6 +61,7 @@ class HECClient(object):
             to save in Splunk
 
         """
+        logger.debug("Saving aggregate reports to Splunk")
         if type(aggregate_reports) == dict:
             aggregate_reports = [aggregate_reports]
 
@@ -122,6 +123,7 @@ class HECClient(object):
             to save in Splunk
 
         """
+        logger.debug("Saving forensic reports to Splunk")
         if type(forensic_reports) == dict:
             forensic_reports = [forensic_reports]
 
