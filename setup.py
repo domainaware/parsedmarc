fe611ac9df22fe9ce3409729df40a717534a4eab
Mike Siegel <msiegel@wayfair.com>
2018-10-10 11:57:41 -0400

    added k version to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-__version__ = "4.2.0"
+__version__ = "4.2.0k"
 description = "A Python package and CLI for parsing aggregate and " \
               "forensic DMARC reports"
 
