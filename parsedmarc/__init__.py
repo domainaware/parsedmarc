66e707bfdff443477f7a88980737028a34899b83
Mike Siegel <msiegel@wayfair.com>
2018-10-10 10:12:34 -0400

    bumping version

--- parsedmarc/__init__.py
+++ parsedmarc/__init__.py
@@ -44,7 +44,7 @@ import imapclient.exceptions
 import dateparser
 import mailparser
 
-__version__ = "4.2.0"
+__version__ = "4.2.0k"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
