7634b9c9d111c3e4dd858d073766ae48a5df9d5f
Sean Whalen <whalenster@gmail.com>
2018-11-26 11:46:33 -0500

    Update venv location in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-. ~/venv/domainaware/bin/activate
+. venv/bin/activate
 pip install -U -r requirements.txt && rstcheck --report warning README.rst && cd docs && make html && cp -r _build/html/* ../../parsedmarc-docs/ && cd .. && flake8 parsedmarc && flake8 tests.py && rm -rf dist/ build/ && python3 setup.py sdist && python3 setup.py bdist_wheel
