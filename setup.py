a3a6e978766e93a13c2f95fd338cbead5889c20a
Sean Whalen <whalenster@gmail.com>
2019-01-13 15:06:39 -0500

    Bump version number to 5.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-__version__ = "5.2.0"
+__version__ = "5.2.1"
 
 description = "A Python package and CLI for parsing aggregate and " \
               "forensic DMARC reports"
