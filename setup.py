2b789c869adfff8e961d051d7006eaaddec071d8
chinskiy <chinskiy93@gmail.com>
2019-10-31 13:08:25 +0200

    add python 3.8 to CI

--- setup.py
+++ setup.py
@@ -74,6 +74,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
