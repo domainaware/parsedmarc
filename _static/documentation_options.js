03ad2a386c2d756c4fcfdcd02dd40526766b3a4f
Sean Whalen <whalenster@gmail.com>
2020-02-17 16:27:12 -0500

    6.9.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.8.2',
+    VERSION: '6.9.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
