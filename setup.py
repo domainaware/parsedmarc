b54bcc91388da1566c7a1bb50b4d25766fa7d3d1
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2021-12-07 12:05:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=["parsedmarc""parsedmarc.resources"],
+    packages=["parsedmarc", "parsedmarc.resources"],
 
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
