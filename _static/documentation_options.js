4e3bc5c312f1a9f6217cc3c7fc29e935fcd25e33
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-01-05 12:21:34 -0500

    7.1.0
    Author:    Sean Whalen <44679+seanthegeek@users.noreply.github.com>

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.0.1',
+    VERSION: '7.1.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
