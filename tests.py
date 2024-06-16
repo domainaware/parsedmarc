c24ca9e7e7b084198b22703696f8e1d3f1760af4
paulp <pphillips99@gmail.com>
2024-06-16 07:41:05 -0500

    fixup and tests to extract_report() to handle documented inputs

--- tests.py
+++ tests.py
@@ -1,12 +1,25 @@
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 
+import os
 import unittest
 from glob import glob
-import os
+
+from lxml import etree
 
 import parsedmarc
 import parsedmarc.utils
 
+
+def minify_xml(xml_string):
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.fromstring(xml_string.encode('utf-8'), parser)
+    return etree.tostring(tree, pretty_print=False).decode('utf-8')
+
+def compare_xml(xml1, xml2):
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree1 = etree.fromstring(xml1.encode('utf-8'), parser)
+    tree2 = etree.fromstring(xml2.encode('utf-8'), parser)
+    return etree.tostring(tree1) == etree.tostring(tree2)
 
 class Test(unittest.TestCase):
     def testBase64Decoding(self):
@@ -25,6 +38,66 @@ class Test(unittest.TestCase):
         subdomain = "e3191.c.akamaiedge.net"
         result = parsedmarc.utils.get_base_domain(subdomain)
         assert result == "c.akamaiedge.net"
+
+    def testExtractReportXMLComparator(self):
+        """Test XML comparator function"""
+        print()
+        xmlnice = open("samples/extract_report/nice-input.xml").read()
+        print(xmlnice)
+        xmlchanged = minify_xml(open("samples/extract_report/changed-input.xml").read())
+        print(xmlchanged)
+        self.assertTrue(compare_xml(xmlnice, xmlnice))
+        self.assertTrue(compare_xml(xmlchanged, xmlchanged))
+        self.assertFalse(compare_xml(xmlnice, xmlchanged))
+        self.assertFalse(compare_xml(xmlchanged, xmlnice))
+        print("Passed!")
+
+    def testExtractReportBytes(self):
+        """Test extract report function for bytes string input"""
+        print()
+        file = "samples/extract_report/nice-input.xml"
+        with open(file, 'rb') as f:
+            data = f.read()
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(data)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportXML(self):
+        """Test extract report function for XML input"""
+        print()
+        file = "samples/extract_report/nice-input.xml"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportGZip(self):
+        """Test extract report function for gzip input"""
+        print()
+        file = "samples/extract_report/nice-input.xml.gz"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportZip(self):
+        """Test extract report function for zip input"""
+        print()
+        file = "samples/extract_report/nice-input.xml.zip"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        print(xmlout)
+        xmlin = minify_xml(open("samples/extract_report/nice-input.xml").read())
+        print(xmlin)
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        xmlin = minify_xml(open("samples/extract_report/changed-input.xml").read())
+        print(xmlin)
+        self.assertFalse(compare_xml(xmlout, xmlin))
+        print("Passed!")
 
     def testAggregateSamples(self):
         """Test sample aggregate/rua DMARC reports"""
