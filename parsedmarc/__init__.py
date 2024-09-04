domainaware/parsedmarc

commit 410663dbcaba019ca3d3744946348b56a635480b

fixup and tests to extract_report() to handle documented inputs (#530)

parsedmarc/__init__.py
@@ -591,14 +591,19 @@ def extract_report(input_):
         str: The extracted text
 
     """
+    def is_base64(s):
+        base64_regex = re.compile(r'^[A-Za-z0-9+/=]+\Z')
+        return bool(base64_regex.match(s))
+
     try:
-        file_object = BytesIO()
-        if type(input_) is str:
-            try:
-                file_object = BytesIO(b64decode(input_))
-            except binascii.Error:
-                pass
-            if file_object is None:
+        file_object = None
+        if isinstance(input_, str):
+            if is_base64(input_):
+                try:
+                    file_object = BytesIO(b64decode(input_))
+                except binascii.Error:
+                    pass
+            else:
                 file_object = open(input_, "rb")
         elif type(input_) is bytes:
             file_object = BytesIO(input_)
@@ -613,7 +618,7 @@ def extract_report(input_):
                 errors='ignore')
         elif header.startswith(MAGIC_GZIP):
             report = zlib.decompress(
-                file_object.getvalue(),
+                file_object.read(),
                 zlib.MAX_WBITS | 16).decode(errors='ignore')
         elif header.startswith(MAGIC_XML) or header.startswith(MAGIC_JSON):
             report = file_object.read().decode(errors='ignore')

tests.py
@@ -1,12 +1,25 @@
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import absolute_import, print_function, unicode_literals
 
+import os
 import unittest
 from glob import glob
-import os
+
+from lxml import etree
 
 import parsedmarc
 import parsedmarc.utils
 
+
+def minify_xml(xml_string):
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.fromstring(xml_string.encode('utf-8'), parser)
+    return etree.tostring(tree, pretty_print=False).decode('utf-8')
+
+def compare_xml(xml1, xml2):
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree1 = etree.fromstring(xml1.encode('utf-8'), parser)
+    tree2 = etree.fromstring(xml2.encode('utf-8'), parser)
+    return etree.tostring(tree1) == etree.tostring(tree2)
 
 class Test(unittest.TestCase):
     def testBase64Decoding(self):
@@ -25,6 +38,66 @@ class Test(unittest.TestCase):
         subdomain = "e3191.c.akamaiedge.net"
         result = parsedmarc.utils.get_base_domain(subdomain)
         assert result == "c.akamaiedge.net"
+
+    def testExtractReportXMLComparator(self):
+        """Test XML comparator function"""
+        print()
+        xmlnice = open("samples/extract_report/nice-input.xml").read()
+        print(xmlnice)
+        xmlchanged = minify_xml(open("samples/extract_report/changed-input.xml").read())
+        print(xmlchanged)
+        self.assertTrue(compare_xml(xmlnice, xmlnice))
+        self.assertTrue(compare_xml(xmlchanged, xmlchanged))
+        self.assertFalse(compare_xml(xmlnice, xmlchanged))
+        self.assertFalse(compare_xml(xmlchanged, xmlnice))
+        print("Passed!")
+
+    def testExtractReportBytes(self):
+        """Test extract report function for bytes string input"""
+        print()
+        file = "samples/extract_report/nice-input.xml"
+        with open(file, 'rb') as f:
+            data = f.read()
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(data)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportXML(self):
+        """Test extract report function for XML input"""
+        print()
+        file = "samples/extract_report/nice-input.xml"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportGZip(self):
+        """Test extract report function for gzip input"""
+        print()
+        file = "samples/extract_report/nice-input.xml.gz"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        xmlin = open("samples/extract_report/nice-input.xml").read()
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        print("Passed!")
+
+    def testExtractReportZip(self):
+        """Test extract report function for zip input"""
+        print()
+        file = "samples/extract_report/nice-input.xml.zip"
+        print("Testing {0}: " .format(file), end="")
+        xmlout = parsedmarc.extract_report(file)
+        print(xmlout)
+        xmlin = minify_xml(open("samples/extract_report/nice-input.xml").read())
+        print(xmlin)
+        self.assertTrue(compare_xml(xmlout, xmlin))
+        xmlin = minify_xml(open("samples/extract_report/changed-input.xml").read())
+        print(xmlin)
+        self.assertFalse(compare_xml(xmlout, xmlin))
+        print("Passed!")
 
     def testAggregateSamples(self):
         """Test sample aggregate/rua DMARC reports"""

samples/extract_report/changed-input.xml
@@ -1,0 +1,592 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<feedback>
+  <report_metadata>
+    <org_name>fred.com</org_name>
+    <email>noreply-dmarc-support@google.com</email>
+    <extra_contact_info>https://support.google.com/a/answer/2466580</extra_contact_info>
+    <report_id>11038226378739404135</report_id>
+    <date_range>
+      <begin>1718236800</begin>
+      <end>1718323199</end>
+    </date_range>
+  </report_metadata>
+  <policy_published>
+    <domain>example.com</domain>
+    <adkim>r</adkim>
+    <aspf>r</aspf>
+    <p>none</p>
+    <sp>none</sp>
+    <pct>100</pct>
+    <np>none</np>
+  </policy_published>
+  <record>
+    <row>
+      <source_ip>209.85.220.69</source_ip>
+      <count>1</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>fail</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.41</source_ip>
+      <count>2</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>gmail.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.90</source_ip>
+      <count>40</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.8.31</source_ip>
+      <count>40</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.8.33</source_ip>
+      <count>33</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.92</source_ip>
+      <count>40</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.110</source_ip>
+      <count>24</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.41</source_ip>
+      <count>12</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>connectivityu.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>2607:f8b0:4864:20::132</source_ip>
+      <count>1</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>google</selector>
+      </dkim>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.8.83</source_ip>
+      <count>36</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.8.96</source_ip>
+      <count>27</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.95</source_ip>
+      <count>25</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.69</source_ip>
+      <count>2252</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>google</selector>
+      </dkim>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.94</source_ip>
+      <count>46</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.8.88</source_ip>
+      <count>37</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.55</source_ip>
+      <count>1</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>google</selector>
+      </dkim>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>54.240.48.93</source_ip>
+      <count>24</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.41</source_ip>
+      <count>23</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.41</source_ip>
+      <count>24</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>fail</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>awbr2rp4egb35wbg4umq4e5dcoe5kc4n</selector>
+      </dkim>
+      <dkim>
+        <domain>amazonses.com</domain>
+        <result>pass</result>
+        <selector>ug7nbtf4gccmlpwj322ax3p6ow6yfsug</selector>
+      </dkim>
+      <spf>
+        <domain>rphvac.com</domain>
+        <result>none</result>
+      </spf>
+    </auth_results>
+  </record>
+  <record>
+    <row>
+      <source_ip>209.85.220.41</source_ip>
+      <count>359</count>
+      <policy_evaluated>
+        <disposition>none</disposition>
+        <dkim>pass</dkim>
+        <spf>pass</spf>
+      </policy_evaluated>
+    </row>
+    <identifiers>
+      <header_from>example.com</header_from>
+    </identifiers>
+    <auth_results>
+      <dkim>
+        <domain>example.com</domain>
+        <result>pass</result>
+        <selector>google</selector>
+      </dkim>
+      <spf>
+        <domain>example.com</domain>
+        <result>pass</result>
+      </spf>
+    </auth_results>
+  </record>
+</feedback>

commit e967778f25044760dfcde200eed94c4f2de29e94

8.15.0
- Fix processing of SMTP-TLS reports ([#549](https://github.com/domainaware/parsedmarc/issues/549)), which broke in commit [410663d ](https://github.com/domainaware/parsedmarc/commit/410663dbcaba019ca3d3744946348b56a635480b)(PR [#530](https://github.com/domainaware/parsedmarc/pull/530))
  - This PR enforced a stricter check for base64-encoded strings, which SMTP TLS reports from Google did not pass
  - Removing the check introduced its own issue, because some file paths were treated as base64-encoded strings
- Create a separate `extract_report_from_file_path()` function for processioning reports based on a file path
- Remove report extraction based on a file path from `extract_report()`

parsedmarc/__init__.py
@@ -34,7 +34,7 @@ from parsedmarc.utils import is_outlook_msg, convert_outlook_msg
 from parsedmarc.utils import parse_email
 from parsedmarc.utils import timestamp_to_human, human_timestamp_to_datetime
 
-__version__ = "8.14.2"
+__version__ = "8.15.0"
 
 logger.debug("parsedmarc v{0}".format(__version__))
 
@@ -585,32 +585,31 @@ def parse_aggregate_report_xml(
             "Unexpected error: {0}".format(error.__str__()))
 
 
-def extract_report(input_):
+def extract_report(content):
     """
-    Extracts text from a zip or gzip file at the given path, file-like object,
+    Extracts text from a zip or gzip file, as a base64-encoded string, file-like object,
     or bytes.
 
     Args:
-        input_: A path to a file, a file like object, or bytes
+        content: report file as a base64-encoded string, file-like object or bytes.
 
     Returns:
         str: The extracted text
 
     """
+    file_object = None
     try:
-        file_object = None
-        if isinstance(input_, str):
+        if isinstance(content, str):
             try:
-                input_ = b64decode(input_, validate=True)
-                file_object = BytesIO(input_)
+                file_object = BytesIO(b64decode(content))
             except binascii.Error:
                 pass
             if file_object is None:
-                file_object = open(input_, "rb")
-        elif type(input_) is bytes:
-            file_object = BytesIO(input_)
+                file_object = open(content, "rb")
+        elif type(content) is bytes:
+            file_object = BytesIO(content)
         else:
-            file_object = input_
+            file_object = content
 
         header = file_object.read(6)
         file_object.seek(0)
@@ -630,8 +629,6 @@ def extract_report(input_):
 
         file_object.close()
 
-    except FileNotFoundError:
-        raise ParserError("File was not found")
     except UnicodeDecodeError:
         file_object.close()
         raise ParserError("File objects must be opened in binary (rb) mode")
@@ -641,6 +638,14 @@ def extract_report(input_):
             "Invalid archive file: {0}".format(error.__str__()))
 
     return report
+
+def extract_report_from_file_path(file_path):
+    """Extracts report from a file at the given file_path"""
+    try:
+        with open(file_path, "rb") as report_file:
+            return extract_report(report_file.read())
+    except FileNotFoundError:
+        raise ParserError("File was not found")
 
 
 def parse_aggregate_report_file(

tests.py
@@ -82,7 +82,7 @@ class Test(unittest.TestCase):
         print()
         file = "samples/extract_report/nice-input.xml.gz"
         print("Testing {0}: " .format(file), end="")
-        xmlout = parsedmarc.extract_report(file)
+        xmlout = parsedmarc.extract_report_from_file_path(file)
         xmlin = open("samples/extract_report/nice-input.xml").read()
         self.assertTrue(compare_xml(xmlout, xmlin))
         print("Passed!")
@@ -92,7 +92,7 @@ class Test(unittest.TestCase):
         print()
         file = "samples/extract_report/nice-input.xml.zip"
         print("Testing {0}: " .format(file), end="")
-        xmlout = parsedmarc.extract_report(file)
+        xmlout = parsedmarc.extract_report_from_file_path(file)
         print(xmlout)
         xmlin = minify_xml(open(
             "samples/extract_report/nice-input.xml").read())