d68a6e561e67fe78fccff6cd467ef434e2f0c454
Sean Whalen <seanthegeek@users.noreply.github.com>
2025-02-03 16:25:04 -0500

    Update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,6 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '8.17.0',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '8.18.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
