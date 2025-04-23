45738ae6881941a52dc7ed4dd6e7411b12e77e7c
miles <miles@bung.cc>
2025-04-24 02:40:03 +0800

    Fix SyntaxError in elastic forensic report (#598)

--- parsedmarc/elastic.py
+++ parsedmarc/elastic.py
@@ -661,11 +661,10 @@ def save_forensic_report_to_elasticsearch(
         try:
             forensic_doc.save()
         except Exception as e:
-            raise ElasticsearchError("Elasticsearch error: {0}".format(e.__str__())
+            raise ElasticsearchError("Elasticsearch error: {0}".format(e.__str__()))
     except KeyError as e:
         raise InvalidForensicReport(
             "Forensic report missing required field: {0}".format(e.__str__()))
-        )
 
 
 def save_smtp_tls_report_to_elasticsearch(
