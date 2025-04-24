d1531b86f2a8e7a5dcfb19f1e98845dc2b86d3b8
Sean Whalen <seanthegeek@users.noreply.github.com>
2025-04-24 11:42:09 -0400

    Sort known_unknown_base_reverse_dns.txt

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ if [  -d "./../parsedmarc-docs" ]; then
   cp -rf build/html/* ../../parsedmarc-docs/
 fi
 cd ..
+sort -o "parsedmarc/resources/maps/known_unknown_base_reverse_dns.txt" "parsedmarc/resources/maps/known_unknown_base_reverse_dns.txt"
 ./sortmaps.py
 python3 tests.py
 rm -rf dist/ build/
